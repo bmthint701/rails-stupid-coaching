91a580c1ca2fe434b4ff963e66cdcbbdab1d77de
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-19 12:25:21 +0900

    complete with tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,7 +10,7 @@ class QuestionsController < ApplicationController
     elsif @question[-1] == '?'
       @answer = 'Silly question, get dressed and go to work!'
     else
-      @answer = "I don't care, get dressed and go to work"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
